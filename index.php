5b02218528e8ef422f5fe07aed3273841869c5df
rotarimaria <mariarotari15@gmail.com>
2026-08-18 15:50:58 +0300

    Actualizare fisiere site

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-declare(strict_types=1);
-
-// Se afișează aceeași pagină ca index.html
-readfile(__DIR__ . '/index.html');
--- lucrari.php
+++
@@ -1,5 +0,0 @@
-<?php
-declare(strict_types=1);
-
-// Se afișează aceeași pagină ca lucrari.html
-readfile(__DIR__ . '/lucrari.html');
